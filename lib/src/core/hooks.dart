252014d903b086febf396e5a1ea4d5b89ee267a2
Kilian Schulte <schultek@in.tum.de>
2022-01-15 14:09:56 +0100

    feat: support non-last unmapped props

--- lib/src/core/hooks.dart
+++ lib/src/core/hooks.dart
@@ -29,6 +29,67 @@ class UnmappedPropertiesHooks extends MappingHooks {
   }
 }
 
+class MappedMap<K, V> {
+  Map<String, K> keys;
+  Map<K, V> map;
+
+  MappedMap(this.keys, this.map);
+}
+
+class RefMap with MapMixin<String, dynamic> {
+  Map<String, dynamic> wrapped;
+
+  List<MappedMap> mapped = [];
+
+  RefMap.of(this.wrapped);
+
+  @override
+  Map<K2, V2> map<K2, V2>(
+      MapEntry<K2, V2> transform(String key, dynamic value)) {
+    var result = <K2, V2>{};
+    var keys = <String, K2>{};
+
+    for (var key in this.keys) {
+      var entry = transform(key, this[key]);
+      keys[key] = entry.key;
+      result[entry.key] = entry.value;
+    }
+
+    mapped.add(MappedMap(keys, result));
+
+    return result;
+  }
+
+  @override
+  operator [](Object? key) {
+    return wrapped[key];
+  }
+
+  @override
+  void operator []=(String key, value) {
+    wrapped[key] = value;
+  }
+
+  @override
+  void clear() {
+    wrapped.clear();
+    for (var map in mapped) {
+      map.map.clear();
+    }
+  }
+
+  @override
+  Iterable<String> get keys => wrapped.keys;
+
+  @override
+  remove(Object? key) {
+    wrapped.remove(key);
+    for (var map in mapped) {
+      map.map.remove(map.keys[key]);
+    }
+  }
+}
+
 class UnusedPropertiesMap with MapMixin<String, dynamic> {
   Map<String, dynamic> wrapped;
   Map<String, dynamic> unused;
@@ -36,7 +97,7 @@ class UnusedPropertiesMap with MapMixin<String, dynamic> {
   String key;
 
   UnusedPropertiesMap.of(this.wrapped, {required this.key})
-      : unused = {...wrapped};
+      : unused = RefMap.of({...wrapped});
 
   @override
   dynamic operator [](Object? key) {
