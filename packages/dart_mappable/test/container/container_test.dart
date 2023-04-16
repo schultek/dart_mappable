7329b56727e825f8ed4d5e5d070fd46cc5d57f94
Kilian Schulte <schultek@in.tum.de>
2023-04-16 14:29:05 +0200

    improve tests

--- packages/dart_mappable/test/container/container_test.dart
+++ packages/dart_mappable/test/container/container_test.dart
@@ -26,6 +26,52 @@ void main() {
           .use(PrimitiveMapper((value) => int.parse(value.toString()) * 10));
 
       expect(container.fromValue<int>('2'), 20);
+
+      expect(container.getAll(), isEmpty);
+    });
+
+    test('link containers', () {
+      var containerA = MapperContainer();
+      var containerB = MapperContainer();
+
+      var mapper = MapperContainer.defaults.unuse<int>()!;
+
+      containerB.use(mapper);
+
+      expect(
+        () => containerA.fromValue<int>('2'),
+        throwsMapperException(MapperException.chain(
+          MapperMethod.decode,
+          '(int)',
+          MapperException.unknownType(int),
+        )),
+      );
+
+      expect(containerB.fromValue<int>('2'), equals(2));
+
+      containerA.link(containerB);
+
+      expect(containerA.fromValue<int>('2'), equals(2));
+    });
+
+    test('iterables', () {
+      var container = MapperContainer();
+
+      expect(
+        container.fromIterable<List<int>>(['2', 1.5, 0]),
+        equals([2, 2, 0]),
+      );
+
+      expect(
+        container.toIterable([DateTime.utc(2000)]),
+        equals(['2000-01-01T00:00:00.000Z']),
+      );
+
+      expect(
+        () => container.toIterable(1),
+        throwsMapperException(
+            MapperException.incorrectEncoding(int, 'Iterable', int)),
+      );
     });
   });
 }
